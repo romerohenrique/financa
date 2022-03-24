05644105f6afc7b715f859da4ac0d540e6be5c88
NAR <romerohenrique08@gmail.com>
2022-03-23 21:48:52 -0300

    mudanças de pastas

--- algaworks-api/src/main/java/com/example/algamoney/api/model/Categoria.java
+++ algaworks-api/src/main/java/com/example/algamoney/api/model/Categoria.java
@@ -5,7 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name="categoria")
@@ -15,7 +14,6 @@ public class Categoria {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
-	@NotNull
 	private String nome;
 	
 	
