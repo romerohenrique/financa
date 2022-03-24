ae6b5cf44ecf59ba2f23695cf79e79e2bb4dcead
NAR <romerohenrique08@gmail.com>
2022-03-23 22:04:07 -0300

    configurando o Fly-way

--- algaworks-api/src/main/java/com/example/algamoney/api/model/Categoria.java
+++ algaworks-api/src/main/java/com/example/algamoney/api/model/Categoria.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name="categoria")
@@ -14,6 +15,7 @@ public class Categoria {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
+	@NotNull
 	private String nome;
 	
 	
